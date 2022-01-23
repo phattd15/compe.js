f0f784b7a48388ef8f7ea962409720e0303d1d54
Phat <futanaristic@gmail.com>
2022-01-23 11:34:14 +0700

    Patch and add extract option

---
+++ src/extractor.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+let data = fs.readFileSync(__dirname + "/../dist/compe.cjs.development.js", {encoding: 'utf-8'});
+var lines = data.split('\n');
+let firstExportIndex = 0;
+for (var i = 0; i < lines.length; i ++) {
+  if (lines[i].startsWith('exports')) {
+    firstExportIndex = i;
+    break;
+  }
+}
+data = lines.slice(4, firstExportIndex).join('\n');
+try {
+  fs.writeFileSync(__dirname + "/lib.txt", data);
+} catch (err) {
+  console.log(err);
+}
