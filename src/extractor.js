da6c96c79d0206d7088c3260f8ea8694dbce2ed4
Phat <futanaristic@gmail.com>
2022-01-24 10:49:51 +0700

    precompress lib script

--- src/extractor.js
+++ src/extractor.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { exec } = require("child_process");
 
 let data = fs.readFileSync(__dirname + "/../dist/compe.cjs.development.js", {encoding: 'utf-8'});
 var lines = data.split('\n');
@@ -11,7 +12,25 @@ for (var i = 0; i < lines.length; i ++) {
 }
 data = lines.slice(4, firstExportIndex).join('\n');
 try {
-  fs.writeFileSync(__dirname + "/lib.txt", data);
+  fs.writeFileSync(__dirname + "/libpre.js", data);
 } catch (err) {
   console.log(err);
 }
+
+function executor(script, cb, cbargs) {
+  console.log(`> ${script}`);
+  exec(script, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(stdout);
+    if (cb) cb(cbargs);
+  });
+}
+
+executor("npx minify " + __dirname + "/libpre.js > " + __dirname + "/lib.txt");
