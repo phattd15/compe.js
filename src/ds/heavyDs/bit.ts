66a611ac803880ce133c674bf7f7ec629306c45c
Phat <futanaristic@gmail.com>
2022-02-19 15:28:43 +0700

    updateMethod to merger

--- src/ds/heavyDs/bit.ts
+++ src/ds/heavyDs/bit.ts
@@ -1,33 +1,33 @@
 class FenwickTree {
   cont: any[];
   identityValue: any;
-  updateMethod: any;
+  merger: any;
   elemCount: number;
   /**
    *
    * @param elemCount The number of elements that the tree supports
    * @param identityValue The null value regarding the operation
-   * @param updateMethod The combination function of node
+   * @param merger The combination function of node
    */
   constructor(
     elemCount: number,
     identityValue: any = 0,
-    updateMethod: any = (a: any, b: any) => a + b
+    merger: any = (a: any, b: any) => a + b
   ) {
     this.identityValue = identityValue;
-    this.updateMethod = updateMethod;
+    this.merger = merger;
     this.elemCount = elemCount;
     this.cont = Array(elemCount + 1).fill(identityValue);
   }
   query(index: number) {
     let res = this.identityValue;
     for (; index >= 0; index = (index & (index + 1)) - 1)
-      res = this.updateMethod(res, this.cont[index]);
+      res = this.merger(res, this.cont[index]);
     return res;
   }
   update(index: number, delta: any) {
     for (; index <= this.elemCount; index |= index + 1)
-      this.cont[index] = this.updateMethod(this.cont[index], delta);
+      this.cont[index] = this.merger(this.cont[index], delta);
   }
 }
 
